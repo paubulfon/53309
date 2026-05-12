ad7c6411dcb0b34aaa7cf7421755c2dfbedf519e
paubulfon <bulfonmariapaula@gmail.com>
2026-05-11 21:41:31 -0300

    Eliminar archivos duplicados

--- CustomComandoListener.js
+++
@@ -1,9 +0,0 @@
-import ComandosListener from "./generated/ComandosListener.js";
-
-export class CustomComandosListener extends ComandosListener {
-
-    enterStat(ctx) {
-        console.log(`Se detectó una: ${ctx.constructor.name}`);
-    }
-
-}
